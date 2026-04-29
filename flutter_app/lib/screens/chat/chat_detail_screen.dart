190cc09386e1223385d050e35291fd95e76d5742
Shubham <shubham@example.com>
2026-04-29 16:12:47 +0530

    Fixed missing imports in ChatDetailScreen to resolve build error

--- flutter_app/lib/screens/chat/chat_detail_screen.dart
+++ flutter_app/lib/screens/chat/chat_detail_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
+import '../../providers/language_provider.dart';
 
 class ChatDetailScreen extends StatefulWidget {
   final Map<String, dynamic> resultData;
